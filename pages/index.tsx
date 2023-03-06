7a7853dd77f40fa3ae0509fb5a20f97f8eac9ad2
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-03-06 20:15:58 +0300

    weather app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Navbar: React.FC = () => {
-  const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }))
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+}
+
+const getWeatherData = async (city: string): Promise<WeatherData | null> => {
+  const apiKey = "9baa53aaf5cdf5ee5ca05b5547934424";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const WeatherDisplay = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [city, setCity] = useState("bursa");
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      const timeString = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-      setTime(timeString);
-    }, 1000)
-    return () => clearInterval(interval);
-  }, [])
+    const fetchData = async () => {
+      const data = await getWeatherData(city);
+      if (data !== null) {
+        setWeatherData(data);
+      }
+    };
+
+    fetchData();
+  }, [city]);
 
   return (
-    <nav>
-      <div className='navbar'>
-        <h1 className='clock'>{time}</h1>
-        <p>Welcome to my nextsite.</p>
-      </div>
-    </nav>
-  )
-}
+    <div>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.name} Weather</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+          <img
+            src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+            alt={weatherData.weather[0].description}
+          />
+        </>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
+};
 
-
-const Content: React.FC = () => {
-  return(
-    <main className='container'>
-      <h1>başlık</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa magni tenetur repudiandae consectetur eveniet, consequatur ipsa ullam. Fuga, voluptatum accusamus.</p>
-    </main>
-  )
-}
-
-
-const Footer: React.FC = () =>{
-  return(
-    <footer>
-      <p>
-        Telifhakları saklıdır
-      </p>
-    </footer>
-  )
-}
-
-const index: React.FC = () =>{
-  return(
-    <>
-      <Navbar />
-      <Content />
-      <Footer />
-    </>
-  )
-}
-
-
-export default index;
+export default WeatherDisplay;
